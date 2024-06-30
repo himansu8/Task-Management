8b03a0675894883070bd5d59a72377dc5aa28acc
himansu8 <himansusekharsahu50@gmail.com>
2024-06-30 23:06:55 +0530

    build file created

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const corsOptions = {
 
 
    app.use(cors(corsOptions));
-  //app.options('*', cors(corsOptions));
+  app.options('*', cors(corsOptions));
 
 // app.get('/',(req,res)=>{
 //     res.status(200).send("server started up fine")
